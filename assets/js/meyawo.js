3db77a4ea2dc98222560164860363c6a1d1d7604
Nair <arju88nair@gmail.com>
2023-07-11 23:47:12 +0100

    Skills and text

--- assets/js/meyawo.js
+++ assets/js/meyawo.js
@@ -36,3 +36,32 @@ $('#nav-toggle').click(function(){
     $(this).toggleClass('is-active')
     $('ul.nav').toggleClass('show');
 });
+
+function toggleContent(paragraphId, linkId) {
+    var paragraph = document.getElementById(paragraphId);
+    var link = document.getElementById(linkId);
+
+    if (paragraph.classList.contains("truncated")) {
+      paragraph.classList.remove("truncated");
+      link.innerText = "Read less";
+    } else {
+      paragraph.classList.add("truncated");
+      link.innerText = "Read more";
+    }
+  }
+
+  document.addEventListener("DOMContentLoaded", function() {
+    var paragraphs = document.getElementsByTagName("p");
+
+    for (var i = 0; i < paragraphs.length; i++) {
+      var paragraph = paragraphs[i];
+      var link = paragraph.querySelector("a");
+
+      if (paragraph.offsetHeight > paragraph.clientHeight) {
+        paragraph.classList.add("truncated");
+        link.style.display = "inline";
+      } else {
+        link.style.display = "none";
+      }
+    }
+  });
